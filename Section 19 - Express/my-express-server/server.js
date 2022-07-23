215dbaf24744d6b22c3e87d04d9abb08cb5d50fe
Michael Spencer <ms53150@gmail.com>
2022-07-23 11:09:13 -0400

    Add route for contact and about page

--- Section 19 - Express/my-express-server/server.js
+++ Section 19 - Express/my-express-server/server.js
@@ -3,8 +3,18 @@ const express = require("express");
 const app = express();
 
 // method from express to say what happens when browser gets in touch with server with get request
+
+// routing
 app.get("/", function (req, res) {
-	res.send("Hello");
+	res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/contact", function (req, res) {
+	res.send("Contact me at: ren@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+	res.send("Hello, my name is Ren. I am a programmer.");
 });
 
 app.listen(3000, function () {
