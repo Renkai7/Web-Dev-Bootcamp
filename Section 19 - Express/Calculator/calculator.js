61b667c9ed3dedf71df2ae0bd3e6cb190ed217dc
Michael Spencer <ms53150@gmail.com>
2022-07-23 14:42:50 -0400

    Create html file and create route for it

--- Section 19 - Express/Calculator/calculator.js
+++ Section 19 - Express/Calculator/calculator.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-	res.send("Hello World");
+	// __dirname allows the html file to be found no matter where its stored on another PC
+	// __dirname is the path for the current file the javascript file is on
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {
