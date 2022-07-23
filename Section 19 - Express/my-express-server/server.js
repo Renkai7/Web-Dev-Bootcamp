ef744885c2d27f3c07351d73a3d65e9fec07d2e0
Michael Spencer <ms53150@gmail.com>
2022-07-23 10:30:44 -0400

    Add response message for home page

--- Section 19 - Express/my-express-server/server.js
+++ Section 19 - Express/my-express-server/server.js
@@ -2,4 +2,11 @@ const express = require("express");
 // function representing the express module
 const app = express();
 
-app.listen(3000);
+// method from express to say what happens when browser gets in touch with server with get request
+app.get("/", function (req, res) {
+	res.send("Hello");
+});
+
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
+});
