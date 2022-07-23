e97ac5c0da4b29e39804f638bca6e453ccd93fa3
Michael Spencer <ms53150@gmail.com>
2022-07-22 20:16:42 -0400

    Install superheroes npm to get hero names

--- Section 18 - NodeJS/intro-to-node/index.js
+++ Section 18 - NodeJS/intro-to-node/index.js
@@ -1,5 +1,13 @@
 // allows access for FileSystem library - can now use FileSystem
-const fs = require("fs");
+// const fs = require("fs");
 
 // creates copy of file - copyFileSync(src, destination)
-fs.copyFileSync("file1.txt", "file2.txt");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// ***** Superhero names
+// get access to module
+var superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
