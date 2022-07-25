3437f15b68ca858ef20d116a8ff50ca96a8f221b
Michael Spencer <ms53150@gmail.com>
2022-07-25 18:45:17 -0400

    Setup react basic project

--- Section 33 - React.js/react-basics/src/index.js
+++ Section 33 - React.js/react-basics/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<div>
-		<h1>Hello world</h1>
-		<p>Hi</p>
+		<h1>Hello World!</h1>
+		<p>This is a paragraph</p>
 	</div>
 );
