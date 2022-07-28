8ba3542f065ddaa4104d8b473ad93675dcea30d7
Michael Spencer <ms53150@gmail.com>
2022-07-27 21:57:31 -0400

    Setup list for component excercise

--- Section 33 - React.js/react-basics/src/index.js
+++ Section 33 - React.js/react-basics/src/index.js
@@ -73,28 +73,43 @@ import ReactDOM from "react-dom/client";
 // );
 
 // Styling Challenge - Change text and text color depending on time of day
-const date = new Date();
-let hour = date.getHours();
+// const date = new Date();
+// let hour = date.getHours();
 
-const customStyle = {
-	color: "",
-};
+// const customStyle = {
+// 	color: "",
+// };
 
-let greeting = "";
+// let greeting = "";
 
-if (hour <= 12) {
-	greeting = "Good morning";
-	customStyle.color = "red";
-} else if (hour <= 18) {
-	greeting = "Good afternoon";
-	customStyle.color = "green";
-} else if (hour > 18 || hour === 0) {
-	greeting = "Good evening";
-	customStyle.color = "blue";
-}
+// if (hour <= 12) {
+// 	greeting = "Good morning";
+// 	customStyle.color = "red";
+// } else if (hour <= 18) {
+// 	greeting = "Good afternoon";
+// 	customStyle.color = "green";
+// } else if (hour > 18 || hour === 0) {
+// 	greeting = "Good evening";
+// 	customStyle.color = "blue";
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+// 	<div>
+// 		{/* Inline styling */}
+// 		<h1 style={customStyle}>{greeting}</h1>
+// 	</div>
+// );
+
+// React Components
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<div>
-		<h1 style={customStyle}>{greeting}</h1>
+		<h1>My Favorite Foods</h1>
+		<ul>
+			<li>Burger</li>
+			<li>Steak</li>
+			<li>Pizza</li>
+		</ul>
 	</div>
 );
